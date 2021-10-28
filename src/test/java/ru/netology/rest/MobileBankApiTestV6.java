8dfa6d9125a47843b0e0bad7f4d4362969ba4635
AlexRV83 <leksrst@gmail.com>
2021-10-28 23:13:45 +0300

    build.gradle modified

--- src/test/java/ru/netology/rest/MobileBankApiTestV6.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV6.java
@@ -3,6 +3,7 @@ package ru.netology.rest;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
@@ -22,15 +23,14 @@ class MobileBankApiTestV6 {
     void shouldReturnDemoAccounts() {
         // Given - When - Then
         // Предусловия
-        given()
+        ValidatableResponse body = given()
                 .spec(requestSpec) // со спецификацией проще (особенно когда много тестов)
                 // Выполняемые действия
                 .when()
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-        ;
+                .statusCode(200);
+
     }
 }
